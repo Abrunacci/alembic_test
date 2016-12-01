71049add005780c5b7045c585872a8d47526f4a8
Alejandro Brunacci <Abrunacci@bitson.com.ar>
2016-12-01 01:48:46 -0300

    almost finish

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 #! /usr/bin/env python
 
 # Standard Lib Imports
-
+import sys
 # Third-Party Imports
 
 # CUSTOM Imports
 from app import run
 
+value = sys.argv[1]
 
 if __name__ == '__main__':
-    run()
+    run(value=value)
